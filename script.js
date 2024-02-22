313a0aaa1acbf6d04e8aee8d29b9d3489f817a84
ShadowVectorZ <rayaudet19@gmail.com>
2024-02-22 10:57:57 -0500

    added function that calls on other functions

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-let add=function(x,y){
-    return x+y
-}
-
-let subtract=function(x,y){
-    return x-y
-}
-
-let multiply=function(x,y){
-    return x*y
-}
-
-let divide=function(x,y){
-    return x/y
-}
-
-
 let firstNumber
-let operator
+let operator 
 let nextNumber
+
+let add=function(firstNumber,secondNumber){
+    return firstNumber+secondNumber
+}
+
+let subtract=function(firstNumber,secondNumber){
+    return firstNumber-secondNumber
+}
+
+let multiply=function(firstNumber,secondNumber){
+    return firstNumber*secondNumber
+}
+
+let divide=function(firstNumber,secondNumber){
+    return firstNumber/secondNumber
+}
+
+
+
+
+let operate= function(firstNumber,operator,secondNumber){
+     switch(operator){
+        case '+': return add(firstNumber,secondNumber);
+        break;
+        case '-': return subtract(firstNumber,secondNumber);
+        break;
+        case '*': return multiply(firstNumber,secondNumber);
+        break;
+        case '/': return divide(firstNumber,secondNumber);
+        break;
+        default: add(firstNumber,secondNumber)
+
+     }
+}
