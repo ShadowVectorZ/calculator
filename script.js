b29d038b8a16e4247b58c6cc3dd51a0ea01b3bb0
ShadowVectorZ <rayaudet19@gmail.com>
2024-02-26 12:17:09 -0500

    added code that shows text on display

--- script.js
+++ script.js
@@ -36,13 +36,35 @@ let operate= function(firstNumber,operator,secondNumber){
 
 let display=document.querySelector('#display')
 let buttons=document.querySelector('#buttonContainer')
-
+let displayValue
+let nextInteger
 buttons.addEventListener('click',(event)=>{
    if (event.target.id==='clear'){
     display.textContent=''
+   
+   }
+   else if (event.target.id==='+'){
+    operator='+'
+   }
+
+   else if (event.target.id==='-'){
+    operator='-'
+   }
+   else if (event.target.id==='*'){
+    operator='*'
+    
+   }
+   else if (event.target.id==='/'){
+    operator='/'
    }
    else if (event.target.tagName==='BUTTON'){
-      display.textContent=event.target.innerText
+     nextInteger=event.target.innerText
+    displayValue='' 
+    String(nextInteger)
+      String(displayValue)
+      display.textContent+=displayValue.concat('',nextInteger)
+      nextInteger=''
+      displayValue=display.textContent
    }
 })
 
